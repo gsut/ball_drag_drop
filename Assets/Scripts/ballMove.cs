94a472b6c0c476d0702b6882834f7615d3251cf0
Gökhan Süt <gokhan.sut@gmail.com>
2022-05-06 20:07:50 +0300

    0.2.0
    Code optimisation & documation
    Edit readme

--- Assets/Scripts/ballMove.cs
+++ Assets/Scripts/ballMove.cs
@@ -4,37 +4,30 @@ using UnityEngine;
 
 public class ballMove : MonoBehaviour
 {
-    private Rigidbody rgPlayer;
-    private float Hiz;
     public LineRenderer mLine;
+    private Rigidbody rigitbodyBall;
+    private float speed;                     
     private bool isPressed;
-    public static bool inGame;
+    private static bool inGame;
     private Vector3 mouse;
     private Vector3 vBall;
     private Vector3 goLine;
     private float xBall, zBall;
     private float hBall;
-    private float x, z, h, u;
     private float distLine;
-
-    Vector2 startPos, endPos, direction; // touch start position, touch end position, swipe direction
-    float touchTimeStart, touchTimeFinish, timeInterval; // to calculate swipe time to sontrol throw force in Z direction
     private float timeCounter;
 
     // Start is called before the first frame update
     void Start()
     {
-        rgPlayer = GetComponent<Rigidbody>();
-        Hiz = 20000;
+        rigitbodyBall = GetComponent<Rigidbody>();
         inGame = false;
         isPressed = false;
         xBall = transform.position.x;
         zBall = transform.position.z;
         mLine.enabled = false;
-        mLine.SetPosition(0, transform.position);
-        mLine.SetPosition(1, transform.position);
-
-
+        mLine.SetPosition(0, transform.position);  // set line renderer start position to ball position
+        mLine.SetPosition(1, transform.position);  // set line renderer end position to ball position
     }
 
 
@@ -44,23 +37,27 @@ public class ballMove : MonoBehaviour
     {
 
         timeCounter += Time.deltaTime;
+
+        // Check every half second (for optimisation)    
         if (timeCounter >= 0.5f)
         {
             if (inGame)
             {
-                vBall = rgPlayer.velocity;
+                // get the speed of the ball
+                vBall = rigitbodyBall.velocity;   
                 hBall = vBall.magnitude;
 
-                Debug.Log(hBall);
-                if (hBall < 1.5f)
+                if (hBall < 1.5f)  // if the ball is too slow 
                 {
-                    rgPlayer.velocity = Vector3.zero;
-                    rgPlayer.constraints = RigidbodyConstraints.FreezeAll;
+                    // stop the ball
+                    rigitbodyBall.velocity = Vector3.zero;
+                    rigitbodyBall.constraints = RigidbodyConstraints.FreezeAll;
+
                     hBall = 0;
                     inGame = false;
                     xBall = transform.position.x;
                     zBall = transform.position.z;
-                    mLine.SetPosition(0, transform.position);
+                    mLine.SetPosition(0, transform.position); // set line renderer start position to ball position
                 }
             }
             else
@@ -69,38 +66,43 @@ public class ballMove : MonoBehaviour
             }
             timeCounter = 0f;
 
+            // If the ball has gone too far, return it to the center to re-roll.
             if ((transform.position.z < -250) || (transform.position.z > 250) || (transform.position.x < -250) || (transform.position.x > 250))
             {
-                rgPlayer.velocity = Vector3.zero;
-                rgPlayer.constraints = RigidbodyConstraints.FreezeAll;
+                // stop the ball
+                rigitbodyBall.velocity = Vector3.zero;
+                rigitbodyBall.constraints = RigidbodyConstraints.FreezeAll;
+
                 transform.position = new Vector3(0f, 0f, 0f);
                 inGame = false;
-                mLine.SetPosition(0, transform.position);
+                mLine.SetPosition(0, transform.position);   // set line renderer start position to ball position
             }
 
         }
 
 
-
-        if (isPressed & (hBall == 0))
+        if (isPressed & (hBall == 0))   // if the mouse is pressed and the speed of the ball is zero
         {
+            // calculate where the mouse clicked on the screen (3D)
             Plane plane = new Plane(Vector3.up, 0);
             float distance;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (plane.Raycast(ray, out distance))
+            if (plane.Raycast(ray, out distance))  // can calculate
             {
+                // get click point
                 mouse = ray.GetPoint(distance);
                 mouse.y = 1f;
-                distLine = Vector3.Distance(mouse, mLine.GetPosition(0));
-                Debug.Log("dist: " + distLine);
-                if ((distLine < 19) & (mouse.z > -20) & (mouse.z < 20) & (mouse.x > -20) & (mouse.x < 20))
+                
+                distLine = Vector3.Distance(mouse, mLine.GetPosition(0)); // calculate line distince
+                if ((distLine < 19) & (mouse.z > -20) & (mouse.z < 20) & (mouse.x > -20) & (mouse.x < 20)) // if mouse click point in ground coordinate
                 {
-                    transform.position = mouse;
+                    transform.position = mouse; // set ball position to mouse click position
                 }
             }
 
-            if ((xBall != transform.position.x) & (zBall != transform.position.z))
+            if ((xBall != transform.position.x) & (zBall != transform.position.z)) // if ball moved from first position
             {
+                // set line renderer end position to mouse click position
                 mLine.SetPosition(1, transform.position);
             }
         }
@@ -113,8 +115,9 @@ public class ballMove : MonoBehaviour
         {
             isPressed = true;
             mLine.enabled = true;
-            rgPlayer.constraints = RigidbodyConstraints.None;
-            rgPlayer.constraints = RigidbodyConstraints.FreezePositionY;
+            // reset the ball constraints for moving
+            rigitbodyBall.constraints = RigidbodyConstraints.None;
+            rigitbodyBall.constraints = RigidbodyConstraints.FreezePositionY;
         }
     }
 
@@ -125,11 +128,15 @@ public class ballMove : MonoBehaviour
             isPressed = false;
             inGame = true;
             mLine.enabled = false;
-            // ilk atis burada yapilmali
+
+            // Set the direction in which the ball will be thrown 
             goLine = new Vector3(xBall - transform.position.x, 1f, zBall - transform.position.z);
-            Hiz = Mathf.Sqrt(Mathf.Pow(Mathf.Abs(transform.position.x - xBall), 2) + Mathf.Pow(Mathf.Abs(transform.position.z - zBall), 2)) * 300;
-            rgPlayer.AddForce(goLine * Time.deltaTime * Hiz, ForceMode.Impulse);
-            //rgPlayer.velocity = goLine * Time.deltaTime * u;
+
+            // set the speed at which the ball will be thrown 
+            speed = Vector3.Distance(mLine.GetPosition(0), mLine.GetPosition(1)) * 300;
+
+            // hit the ball
+            rigitbodyBall.AddForce(goLine * Time.deltaTime * speed, ForceMode.Impulse);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ball_drag_drop
-Unity 3D Ball Drag Drop With Shader Material
+Unity 3D Line Renderer Material Effects with Shader Graph
+include Ball Drag Drop 
+
+Unity Editor : 2021.3.1f1
+
+https://youtu.be/H8Ln1kNQ_Hs
+
+
+Qulvar Game Studio
+Gokhan Sut
+gokhan.sut@qulvar.com
